409a4638f4c4bdb172b277ccb12bea0fd7ac88db
league <maddoxseul@sbsd.net>
2024-03-22 16:56:31 -0700

    shrek is not as chuggie as bobby

--- _01_function_writing/_c_obedient_turtle.py
+++ _01_function_writing/_c_obedient_turtle.py
@@ -8,9 +8,32 @@ import turtle
 if __name__ == '__main__':
     # TODO)
     #   1. Create a turtle.
-    #   2. Write 3 method definitions for drawing a square, triangle, and
+    #   2. Write 3 method definitions for drawing a square, triangle, megagon, and
     #      circle.
-    #   3. Ask the user for the for a shape to draw.
+    #   3. Ask the user for a shape to draw.
     #   4. Draw the appropriate shape depending on their answer (call the right
     #      function)
-    pass
+    shrekyduckiee = turtle.Turtle()
+    mega_shapes = simpledialog.askstring(title='shrek', prompt='what shape would you like to draw, triangle, square, megagon, circle')
+    if mega_shapes == 'megagon':
+        for i in range(1000000):
+            shrekyduckiee.speed(0)
+            shrekyduckiee.circle(1000)
+            shrekyduckiee.forward(10)
+    elif mega_shapes == 'square':
+        for i in range(4):
+            shrekyduckiee.speed(1)
+            shrekyduckiee.right(90)
+            shrekyduckiee.forward(500)
+    elif mega_shapes == 'circle':
+        shrekyduckiee.speed(1)
+        shrekyduckiee.circle(1000)
+
+
+
+
+
+
+
+
+
