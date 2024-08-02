55aaff1cd45a3f732bf8a2ffa76e80b1fdd03572
WallyWest74422 <ashaymoorjani@gmail.com>
2024-08-02 16:55:31 -0700

    nickelodeon graham is god teacher

--- _03_nested_loops/for_loop_gauntlet/for_loop_gauntlet.py
+++ _03_nested_loops/for_loop_gauntlet/for_loop_gauntlet.py
@@ -5,4 +5,174 @@ https://central.jointheleague.org/levels/Level0/Recipes/ForLoopGauntlet.html
 
 
 if __name__ == '__main__':
-    pass
+    #2
+    # for i in range(0, 101):
+    #     print(str(i))
+    #     if i % 2 == 0:
+    #         print(i)
+    #     else:
+    #         print("sus")
+
+    #3
+    # for i in range(0, 501):
+    #     print(str(i))
+    #     if i % 2 == 0:
+    #         print(i)
+    #     else:
+    #         print("sus")
+
+    #4
+    #for i in range(0, 501):
+    #   print(str(i))
+    #    if i % 2 == 0:
+    #        print(i)
+    #        print("sus")
+
+    #5
+    # for i in range(777, 7):
+    #     print(str(i))
+    #     if i % 2 == 0:
+    #         print(i)
+    #     else:
+    #         print("sus")
+
+    #6
+    born = 2012
+    year = 2024
+    difference = year - born
+    numba = 12
+    for i in range(difference):
+        print('I was ' + (str(i)) + ' years old in ' + (str(born +  i)) + "")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
